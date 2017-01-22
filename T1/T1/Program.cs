e763eb34b5261a7eabaf1884c67b040974b80e9d
Jani <K1542@student.jamk.fi>
2017-01-22 15:54:14 +0200

    Talteen2

--- T1/T1/Program.cs
+++ T1/T1/Program.cs
@@ -6,27 +6,55 @@ namespace H2T1
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[5];
-            int i, sum=0;
-  
-            Console.Write("Input 10 elements in the array :\n");  
-            for(i=0; i<5; i++)  
-            {  
-	            Console.Write("element - {0} : ",i);
-	            arr[i] = Convert.ToInt32(Console.ReadLine());  		
-            }
-            Console.Write("\nElements in array are: ");  
-            for(i=0; i<5; i++)  
-            {  
-                Console.Write("{0}  ", arr[i]);  
-            }
-            for (i = 0; i < arr.Length; i++)
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[arr1.Length - 1];
+            int i, j, tmp, sum=0;
+
+            Console.Write("Input elements in the array :\n");
+            for (i = 0; i < arr1.Length; i++)
             {
-                sum += arr[1,4];
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
             }
-            Console.Write("\n");
-            Console.Write("Summa: " + sum);
+
+            for (i = 0; i < arr1.Length; i++)
+            {
+                for (j = i + 1; j < arr1.Length; j++)
+                {
+                    if (arr1[j] < arr1[i])
+                    {
+                        tmp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = tmp;
+                    }
+                }
+            }
+            
+            Console.Write("\nElements of array in sorted ascending order:\n");
+            for (i = 0; i < arr1.Length; i++)
+            {
+                Console.Write("{0}  ", arr1[i]);
+            }
+
+            // Pienimmän numeron poisto
+            for (i = 0; i < 0; i++)
+            {
+                // indices in new array before the add position remains the same
+                arr2[i] = arr1[i];
+
+            }
+            for (j = 0; j <= arr2.Length - 1; j++)
+            {
+                // indices in new array after the del position change by 1(move left)
+                arr2[j] = arr1[j + 1];
+            }
+
+            for (i = 0; i < arr2.Length; i++)
+            {
+                sum += arr2[i];
+            }
+            Console.Write("\nSumma: " + sum);
             Console.ReadLine();
-      }
         }
-}
+    }
+        }
