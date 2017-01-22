b0260d95422bae585249e273898dc7d1a9988929
Jani <K1542@student.jamk.fi>
2017-01-22 16:06:26 +0200

    Toimii

--- T1/T1/Program.cs
+++ T1/T1/Program.cs
@@ -7,53 +7,33 @@ namespace H2T1
         static void Main(string[] args)
         {
             int[] arr1 = new int[5];
-            int[] arr2 = new int[arr1.Length - 1];
-            int i, j, tmp, sum=0;
+            int i, j, tmp, sum=0, max, min;
 
-            Console.Write("Input elements in the array :\n");
+            Console.Write("Anna pisteitä:\n");
             for (i = 0; i < arr1.Length; i++)
             {
-                Console.Write("element - {0} : ", i);
+                Console.Write("Pisteet: ");
                 arr1[i] = Convert.ToInt32(Console.ReadLine());
             }
-
+            max = arr1[0];
+            min = arr1[0];
             for (i = 0; i < arr1.Length; i++)
             {
-                for (j = i + 1; j < arr1.Length; j++)
+                if (arr1[i] > max)
                 {
-                    if (arr1[j] < arr1[i])
-                    {
-                        tmp = arr1[i];
-                        arr1[i] = arr1[j];
-                        arr1[j] = tmp;
-                    }
+                    max = arr1[i];
+                }
+                if (arr1[i] < min)
+                {
+                    min = arr1[i];
                 }
             }
-            
-            Console.Write("\nElements of array in sorted ascending order:\n");
             for (i = 0; i < arr1.Length; i++)
             {
-                Console.Write("{0}  ", arr1[i]);
+                sum += arr1[i];
             }
-
-            // Pienimmän numeron poisto
-            for (i = 0; i < 0; i++)
-            {
-                // indices in new array before the add position remains the same
-                arr2[i] = arr1[i];
-
-            }
-            for (j = 0; j <= arr2.Length - 1; j++)
-            {
-                // indices in new array after the del position change by 1(move left)
-                arr2[j] = arr1[j + 1];
-            }
-
-            for (i = 0; i < arr2.Length; i++)
-            {
-                sum += arr2[i];
-            }
-            Console.Write("\nSumma: " + sum);
+            int TotalSum = sum - max - min;
+            Console.Write("\nSumma: " + TotalSum);
             Console.ReadLine();
         }
     }
