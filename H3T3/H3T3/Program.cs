620a64c15f34028b632a8addc3403adc7e4f741e
Jani <K1542@student.jamk.fi>
2017-01-29 23:55:13 +0200

    Toimii

---
+++ H3T3/H3T3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisio tv = new Televisio();
+            tv.Malli = "LG";
+            tv.Väri = "Musta";
+            tv.Päällä = true;
+            tv.Kanava = 3;
+            tv.Ohjelma = "Putous";
+
+            tv.PrintData();
+            Console.ReadLine();
+
+            tv.KanavaYlös();
+            tv.PrintData();
+            Console.ReadLine();
+
+            tv.KanavaYlös();
+            tv.KanavaYlös();
+            tv.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ H3T3/H3T3/Televisio.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T3
+{
+    class Televisio
+    {
+        public string Malli { get; set; }
+        public string Väri { get; set; }
+        public int Kanava { get; set; }
+        public bool Päällä { get; set; }
+        public string Ohjelma { get; set; }
+
+        // don't create any constructor, so default one will be used
+
+        // method to give more speed
+        public void KanavaYlös()
+        {
+            Kanava += 1;
+            if (Kanava == 4)
+            {
+                Ohjelma = "Vain Elämää";
+            }
+            else if (Kanava == 5)
+            {
+                Ohjelma = "Muumit";
+            }
+            else if (Kanava == 6)
+            {
+                Ohjelma = "Simpsonit";
+            }
+            else
+            {
+                Ohjelma = "Ostos-tv";
+            }
+        }
+
+        // method to slow down
+        public void KanavaAlas()
+        {
+            Kanava -= 1;
+            if (Kanava == 1)
+            {
+                Ohjelma = "Uutisvuoto";
+            }
+            else if (Kanava == 2)
+            {
+                Ohjelma = "Game of Thrones";
+            }
+            else
+            {
+                Ohjelma = "Ostos-tv";
+            }
+
+        }
+
+        // method to display car data
+        public void PrintData()
+        {
+            Console.WriteLine("Telkkarin tietoja : ");
+            Console.WriteLine("- Malli : " + Malli);
+            Console.WriteLine("- Väri : " + Väri);
+            Console.WriteLine("- Virta päällä : " + Päällä);
+            Console.WriteLine("- Kanava : " + Kanava);
+            Console.WriteLine("- Nykyinen ohjelma : " + Ohjelma);
+        }
+
+        // destructor
+        ~Televisio()
+        {
+            Console.WriteLine("Car object destroyed.");
+        }
+    }
+}
