ec8e70d57c4eb1b2d945bc6f6b307cb65d23750a
Jani <K1542@student.jamk.fi>
2017-01-30 00:16:59 +0200

    Kesken

---
+++ H3T4/H3T4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Speed = 160;
+            vehicle.Tyres = 4;
+
+            vehicle.PrintData();
+            Console.ReadLine();
+
+            vehicle.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ H3T4/H3T4/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T4
+{
+    class Vehicle
+    {
+        public static string Name = "Toyota";
+        public int Tyres { get; set; }
+        public int Speed { get; set; }
+        private string[] strings = new string[100];
+        public void Accelerate()
+        {
+            Speed += 5;
+        }
+        public void Brake()
+        {
+            Speed -= 5;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Car data : ");
+            Console.WriteLine("- model : " + Name);
+            Console.WriteLine("- Tyres : " + Tyres);
+            Console.WriteLine("- speed : " + Speed);
+        }
+        ~Vehicle()
+        {
+            Console.WriteLine("Car object destroyed.");
+        }
+    }
+}
