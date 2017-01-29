5883b3ebbb553659d7bba7544d5a22459b263bba
Jani <K1542@student.jamk.fi>
2017-01-29 23:35:25 +0200

    Toimii

---
+++ H3T2/H3T2/Pesukone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T2
+{
+    class Pesukone
+    {
+        public string Malli { get; set; }
+        public int Temp { get; set; }
+        public bool Päällä { get; set; }
+        public string Ohjelma { get; set; }
+        public int Ajastus { get; set; }
+        public bool Luukku { get; set; }
+
+       public void State()
+        {
+            Päällä = true;
+            Luukku = true;
+        }
+        public void Keskeytä()
+        {
+            Päällä = false;
+            Luukku = true;
+        }
+        public void Ohjelma2()
+        {
+            Ohjelma = "Tehopesu";
+            Temp = 60;
+            Ajastus = 0;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Pesukone : ");
+            Console.WriteLine("- Malli : " + Malli);
+            Console.WriteLine("- Pesuohjelma : " + Ohjelma);
+            Console.WriteLine("- Päällä : " + Päällä);
+            Console.WriteLine("- Ajastin(min) : " + Ajastus);
+            Console.WriteLine("- Luukku auki : " + Luukku);
+        }
+
+        // destructor
+        ~Pesukone()
+        {
+            Console.WriteLine("Car object destroyed.");
+        }
+    }
+}
---
+++ H3T2/H3T2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesukone pesukone = new Pesukone();
+            pesukone.Malli = "Whirlpool 3000";
+            pesukone.Päällä = true;
+            pesukone.Ohjelma = "Pikapesu";
+            pesukone.Temp = 50;
+            pesukone.Ajastus = 30;
+            pesukone.Luukku = false;
+
+            pesukone.PrintData();
+            Console.ReadLine();
+
+            pesukone.Keskeytä();
+            pesukone.PrintData();
+            Console.ReadLine();
+
+            pesukone.Ohjelma2();
+            pesukone.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
