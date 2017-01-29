43413971f37ca26dafc083a7a038ce1b473ef397
Jani <K1542@student.jamk.fi>
2017-01-29 22:57:43 +0200

    Toimii

---
+++ H3T1/H3T1/Kiuas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T1
+{
+    class Kiuas
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Humidity { get; set; }
+        public int Temp { get; set; }
+        public bool State { get; set; }
+        public int DoorCount { get; set; }
+        public void TempUP()
+        {
+            Temp += 5;
+        }
+        public void TempDown()
+        {
+            Temp -= 5;
+        }
+
+        public void HumiditydUP()
+        {
+            Humidity += 5;
+        }
+        public void HumidityDown()
+        {
+            Humidity -= 5;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Kiukaan tietoja: ");
+            Console.WriteLine("- Päällä: " + State);
+            Console.WriteLine("- Lämpötila : " + Temp);
+            Console.WriteLine("- Kosteus : " + Humidity);
+        }
+        ~Kiuas()
+        {
+            Console.WriteLine("Kiuas tuhottu");
+        }
+    }
+}
---
+++ H3T1/H3T1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas = new Kiuas();
+            kiuas.State = true;
+            kiuas.Temp = 80;
+            kiuas.Humidity = 70;
+            kiuas.PrintData();
+            Console.ReadLine();
+            kiuas.TempUP();
+            kiuas.HumiditydUP();
+            kiuas.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
