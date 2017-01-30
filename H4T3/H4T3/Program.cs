8220380546eff08a3890fdb77b5a4a0a03968dc6
Jani Järvinen <K1542@student.jamk.fi>
2017-01-30 19:56:19 +0200

    Tehty

--- H4T3/H4T3/Program.cs
+++ H4T3/H4T3/Program.cs
@@ -10,6 +10,20 @@ namespace H4T3
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
+            employee.Name = "Kirsi Kermit ";
+            employee.Profession = "Teacher ";
+            employee.Salary = 3000;
+            Console.WriteLine(employee.ToString());
+            Boss boss = new Boss();
+            boss.Name = "Ari Rantala ";
+            boss.Profession = "Headmaster ";
+            boss.Salary = 9001;
+            boss.Car = "Lamborghini ";
+            boss.Bonus = 2000;
+            Console.WriteLine(boss.ToString());
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
         }
     }
 }
