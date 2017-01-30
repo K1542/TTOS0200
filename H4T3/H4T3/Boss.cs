0246efdfeadc75e0b6c5d82813ef1cd3e33e9d99
Jani Järvinen <K1542@student.jamk.fi>
2017-01-30 19:49:04 +0200

    Jatkettu

---
+++ H4T3/H4T3/Boss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4T3
+{
+    class Boss : Employee
+    {
+        public string Car { get; set; }
+        public double Bonus { get; set; }
+
+        public Boss()
+        {
+
+        }
+        public Boss(string name, string profession, double salary, string car, double bonus): base(name, profession, salary)
+        {
+            Car = car;
+            Bonus = bonus;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Car: " + Car + "Bonus: " + Bonus;
+
+        }
+
+
+
+
+    }
+}
