68cd3354327fc1c4c1f993574608a161f1e55fc2
Jani Järvinen <K1542@student.jamk.fi>
2017-01-30 19:41:57 +0200

    Kesken

---
+++ H4T3/H4T3/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4T3
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(string name, string profession,double salary)
+        {
+            Name        = name;
+            Profession  = profession;
+            Salary      = salary;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "Profession: " + Profession + "Salary: " + Salary;
+            
+        }
+
+
+
+
+    }
+}
