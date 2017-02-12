4b451faeed7aea98c1d79789b1eaa2669561c8a2
Jani <K1542@student.jamk.fi>
2017-02-12 13:34:09 +0200

    Toimii

---
+++ H5T3/H5T3/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5T3
+{
+    class Company
+    {
+        private Stack<Worker> workers;
+
+        public Company()
+        {
+            workers = new Stack<Worker>();
+        }
+        public void AddEmployee(string name)
+        {
+            workers.Push(new Worker(name));
+        }
+        public void FireLast()
+        {
+            System.Console.Write("Worker '");
+            workers.Pop().PrintData();
+            System.Console.Write("' kicked out\n");
+        }
+        public void PrintData()
+                {
+                    foreach (Worker worker in workers)
+                    {
+                        System.Console.Write("Worker name ");
+                        worker.PrintData();
+                        System.Console.Write("\n");
+                    }
+                }
+    }
+}
---
+++ H5T3/H5T3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5T3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Company company = new Company();
+            while (true)
+            {
+                int selection = -1;
+                Console.Write("1. Hire. 2. Fire. 3. List 0. Quit: ");
+                bool status = int.TryParse(Console.ReadLine(), out selection);
+                if (status)
+                {
+                    switch (selection)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                            Console.Write("Give name for new worker: ");
+                            string name = Console.ReadLine();
+                            company.AddEmployee(name);
+                            break;
+                        case 2:
+                            company.FireLast();
+                            break;
+                        case 3:
+                            company.PrintData();
+                            break;
+
+                        default:
+                            Console.WriteLine("Try again");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You must give number");
+                }
+            }
+        }
+    }
+}
