018db3340ae4d4e46cb4b6fee1d4bee407fd4fbb
K1542 <K1542@student.jamk.fi>
2017-03-19 19:42:38 +0200

    Create Employee.cs

---
+++ H9/T6/Employee.cs
@@ -0,0 +1,16 @@
+namespace H9T6
+{
+    public class Employee
+    {
+        public string Firstname
+        {
+            get;
+            set;
+        }
+        public string Lastname
+        {
+            get;
+            set;
+        }
+    }
+}
